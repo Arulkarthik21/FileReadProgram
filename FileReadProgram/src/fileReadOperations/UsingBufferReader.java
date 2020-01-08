582104242628f2ba70d2843d80b3de859a31b1f7
Arul.Inmaa <Arul.Inmaa@NASSCHNGY-005DT>
2020-01-08 12:02:02 +0530

    Initial commit

---
+++ FileReadProgram/src/fileReadOperations/UsingBufferReader.java
@@ -0,0 +1,13 @@
+package fileReadOperations;
+
+public class UsingBufferReader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String location="";
+		
+
+	}
+
+}
